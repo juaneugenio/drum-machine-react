fb11336cde0f46198bd80e6016c2dda3b645bc79
juaneugenio <leuviah77@gmail.com>
2025-02-11 17:22:51 +0100

    new deploy fix

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,13 +1,13 @@
 /** @format */
 
 export const DRUM_PADS = [
-	{ key: "Q", id: "Heater-1", audio: "/sounds/Heater 1.mp3" },
-	{ key: "W", id: "Heater-2", audio: "/sounds/Heater 2.mp3" },
-	{ key: "E", id: "Heater-3", audio: "/sounds/Heater 3.mp3" },
-	{ key: "A", id: "Heater-4", audio: "/sounds/Heater 4.1.mp3" },
-	{ key: "S", id: "Clap", audio: "/sounds/Heater 6.mp3" },
-	{ key: "D", id: "Open-HH", audio: "/sounds/Dsc Oh.mp3" },
-	{ key: "Z", id: "Kick-n-Hat", audio: "/sounds/Kick n Hat.mp3" },
-	{ key: "X", id: "Kick", audio: "/sounds/RP4 Kick 1.mp3" },
-	{ key: "C", id: "Closed-HH", audio: "/sounds/Cev H2.mp3" },
+	{ key: "Q", id: "Heater-1", audio: "sounds/Heater 1.mp3" },
+	{ key: "W", id: "Heater-2", audio: "sounds/Heater 2.mp3" },
+	{ key: "E", id: "Heater-3", audio: "sounds/Heater 3.mp3" },
+	{ key: "A", id: "Heater-4", audio: "sounds/Heater 4.1.mp3" },
+	{ key: "S", id: "Clap", audio: "sounds/Heater 6.mp3" },
+	{ key: "D", id: "Open-HH", audio: "sounds/Dsc Oh.mp3" },
+	{ key: "Z", id: "Kick-n-Hat", audio: "sounds/Kick n Hat.mp3" },
+	{ key: "X", id: "Kick", audio: "sounds/RP4 Kick 1.mp3" },
+	{ key: "C", id: "Closed-HH", audio: "sounds/Cev H2.mp3" },
 ];
