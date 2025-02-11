ea6948a0c23c075a625ff162471422c433a56bed
juaneugenio <leuviah77@gmail.com>
2025-02-11 17:10:26 +0100

    fixing deploy

--- vite.config.js
+++ vite.config.js
@@ -4,14 +4,26 @@ import { defineConfig } from "vitest/config";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-	base: '/drum-machine/',  // Add this line
-	plugins: [react()],
-	test: {
-		environment: "jsdom",
-		globals: true,
-		setupFiles: "./src/test/setup.js",
-		coverage: {
-			reporter: ["text", "json", "html"],
-		},
-	},
+  base: '/drum-machine/',
+  plugins: [react()],
+  build: {
+    outDir: 'dist',
+    emptyOutDir: true,
+    sourcemap: true,
+    rollupOptions: {
+      output: {
+        entryFileNames: `assets/[name].js`,
+        chunkFileNames: `assets/[name].js`,
+        assetFileNames: `assets/[name].[ext]`
+      }
+    }
+  },
+  test: {
+    environment: "jsdom",
+    globals: true,
+    setupFiles: "./src/test/setup.js",
+    coverage: {
+      reporter: ["text", "json", "html"],
+    },
+  },
 });
