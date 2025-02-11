17f2f11f29f6ffa3bee4973ae681ba2717f7d459
juaneugenio <leuviah77@gmail.com>
2025-02-11 17:34:41 +0100

    fixing base url

--- vite.config.js
+++ vite.config.js
@@ -4,30 +4,30 @@ import { defineConfig } from "vitest/config";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: '/drum-machine/',
-  plugins: [react()],
-  build: {
-    outDir: 'dist',
-    emptyOutDir: true,
-    assetsDir: 'assets',
-    rollupOptions: {
-      output: {
-        assetFileNames: (assetInfo) => {
-          const info = assetInfo.name.split('.');
-          const ext = info[info.length - 1];
-          return `assets/${info[0]}.[hash].${ext}`;
-        },
-        chunkFileNames: 'assets/[name].[hash].js',
-        entryFileNames: 'assets/[name].[hash].js'
-      }
-    }
-  },
-  test: {
-    environment: "jsdom",
-    globals: true,
-    setupFiles: "./src/test/setup.js",
-    coverage: {
-      reporter: ["text", "json", "html"],
-    },
-  },
+	base: "/drum-machine-react/",
+	plugins: [react()],
+	build: {
+		outDir: "dist",
+		emptyOutDir: true,
+		assetsDir: "assets",
+		rollupOptions: {
+			output: {
+				assetFileNames: (assetInfo) => {
+					const info = assetInfo.name.split(".");
+					const ext = info[info.length - 1];
+					return `assets/${info[0]}.[hash].${ext}`;
+				},
+				chunkFileNames: "assets/[name].[hash].js",
+				entryFileNames: "assets/[name].[hash].js",
+			},
+		},
+	},
+	test: {
+		environment: "jsdom",
+		globals: true,
+		setupFiles: "./src/test/setup.js",
+		coverage: {
+			reporter: ["text", "json", "html"],
+		},
+	},
 });
